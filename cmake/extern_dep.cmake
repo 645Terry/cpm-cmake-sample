e4382f5788f6d4f432282760245f12208d7e5797
yuzw <zhaowei.yu@phigent.ai>
2022-12-07 17:00:06 +0800

    new version

--- cmake/extern_dep.cmake
+++ cmake/extern_dep.cmake
@@ -16,8 +16,8 @@ install(DIRECTORY ${gflags_SOURCE_DIR}/lib
 CPMAddPackage(
   NAME googletest   # 添加依赖库：googletest
   GITHUB_REPOSITORY google/googletest # GitHub仓库是：google/googletest，CPM会在GitHub网站找到该仓库，并得到对应的仓库链接：https://github.com/google/googletest
-  GIT_TAG release-1.8.1 # Git标签是release-1.8.1
-  VERSION 1.8.1 # 版本号是1.8.1
+  GIT_TAG release-1.12.1 # Git标签是release-1.8.1
+  VERSION 1.12.1 # 版本号是1.8.1
   OPTIONS  # 可选参数包括：INSTALL_GTEST：关，gtest_force_shared_crt，开
       "INSTALL_GTEST OFF"
       "gtest_force_shared_crt ON"
